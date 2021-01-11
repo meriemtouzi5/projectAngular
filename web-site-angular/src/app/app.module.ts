a29487d4bb9e5eae7c6df73c268ff01015037519
meriemtouzi5 <meriemtouzi5@gmail.com>
2021-01-11 12:20:07 +0100

    add system routage in app

--- web-site-angular/src/app/app.module.ts
+++ web-site-angular/src/app/app.module.ts
@@ -3,15 +3,37 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AfficheDataComponent } from './affiche-data/affiche-data.component';
+import { FormComponent } from './form/form.component';
+import { RouterModule,Routes, RoutesRecognized } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+const routes:Routes=[
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'contact',
+    component:FormComponent
+  },
+  {
+    path:'list',
+    component:AfficheDataComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AfficheDataComponent
+    AfficheDataComponent,
+    FormComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule 
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
